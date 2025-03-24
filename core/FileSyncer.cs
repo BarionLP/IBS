a880a5b2d47890396b6df63c3e66d5a6ac31ecd7
Barion <ametrin.studios@gmail.com>
2025-03-24 14:30:51 +0100

    adjustments

--- core/FileSyncer.cs
+++ core/FileSyncer.cs
@@ -25,15 +25,20 @@ public static class FileSyncer
         {
             var relativeOriginPath = directory.GetRelativePath(config.OriginInfo);
             var files = GetFiles(directory);
-            var backupFiles = config.BackupInfos.Select(b => (location: b, files: GetFiles(b.Directory(relativeOriginPath)).Where(f => f.Extension is not ".deleted").ToList())).ToArray();
+
+            var backupFiles = config.BackupInfos.Select(b =>
+            {
+                var dir = b.Directory(relativeOriginPath);
+                dir.CreateIfNotExists();
+                return (location: b, files: GetFiles(dir).Where(static f => f.Extension is not ".deleted").ToList());
+            }).ToArray();
 
             foreach (var file in files)
             {
                 var relativeFilePath = file.GetRelativePath(config.OriginInfo);
                 foreach (var backup in backupFiles)
                 {
-                    Console.WriteLine($"Synced {relativeFilePath}");
-                    //SyncFile(file, backup.location.File(relativeFilePath));
+                    SyncFile(file, backup.location.File(relativeFilePath));
                     backup.files.RemoveAll(f => f.GetRelativePath(backup.location) == relativeFilePath);
                 }
             }
@@ -41,7 +46,7 @@ public static class FileSyncer
             foreach (var file in backupFiles.SelectMany(static s => s.files))
             {
                 Console.WriteLine($"marked {file.Name} deleted");
-                //file.MoveTo($"{file.Name}.deleted");
+                file.MoveTo($"{file.Name}.deleted");
             }
 
             foreach (var sub in directory.EnumerateDirectories("*", SearchOption.TopDirectoryOnly))
@@ -59,7 +64,8 @@ public static class FileSyncer
 
         if (!to.Exists || !AreFilesInSync(from, to))
         {
-            from.CopyTo(to);
+            from.CopyTo(to, overwrite: true);
+            Console.WriteLine($"Synced {from.Name}");
         }
     }
 
