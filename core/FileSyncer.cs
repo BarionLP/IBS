c744ffabfc9316b00783940b0de63243ee2965c7
Barion <ametrin.studios@gmail.com>
2025-03-24 14:17:17 +0100

    fixes

--- core/FileSyncer.cs
+++ core/FileSyncer.cs
@@ -19,27 +19,38 @@ public static class FileSyncer
 
     public static void AdvancedSync(BackupConfig config)
     {
-        var origin = config.OriginInfo.EnumerateFiles("*", SearchOption.AllDirectories).Where(config.ShouldInclude).GetEnumerator();
-        var backups = config.BackupInfos.Select(b => b.EnumerateFiles("*", SearchOption.AllDirectories).Where(config.ShouldInclude).GetEnumerator()).ToArray();
+        Sync(config.OriginInfo);
 
-        foreach (var backup in backups)
+        void Sync(DirectoryInfo directory)
         {
-            backup.MoveNext();
-        }
+            var relativeOriginPath = directory.GetRelativePath(config.OriginInfo);
+            var files = GetFiles(directory);
+            var backupFiles = config.BackupInfos.Select(b => (location: b, files: GetFiles(b.Directory(relativeOriginPath)).Where(f => f.Extension is not ".deleted").ToList())).ToArray();
 
-        while (origin.MoveNext())
-        {
-            var relativePath = origin.Current.GetRelativePath(config.OriginInfo);
-            foreach (var backup in backups)
+            foreach (var file in files)
             {
-                if (backup.Current.FullName.EndsWith(relativePath))
+                var relativeFilePath = file.GetRelativePath(config.OriginInfo);
+                foreach (var backup in backupFiles)
                 {
-                    //SyncFile(origin.Current, backup.Current);
-                    Console.WriteLine($"Synced {origin.Current.FullName}");
-                    backup.MoveNext();
+                    Console.WriteLine($"Synced {relativeFilePath}");
+                    //SyncFile(file, backup.location.File(relativeFilePath));
+                    backup.files.RemoveAll(f => f.GetRelativePath(backup.location) == relativeFilePath);
                 }
             }
+
+            foreach (var file in backupFiles.SelectMany(static s => s.files))
+            {
+                Console.WriteLine($"marked {file.Name} deleted");
+                //file.MoveTo($"{file.Name}.deleted");
+            }
+
+            foreach (var sub in directory.EnumerateDirectories("*", SearchOption.TopDirectoryOnly))
+            {
+                Sync(sub);
+            }
         }
+
+        IEnumerable<FileInfo> GetFiles(DirectoryInfo directory) => directory.Exists ? directory.EnumerateFiles("*", SearchOption.TopDirectoryOnly).Where(config.ShouldInclude) : [];
     }
 
     private static void SyncFile(FileInfo from, FileInfo to)
