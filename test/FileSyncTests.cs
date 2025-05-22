4e826b0a13de5a12267d304681a7b726b7d13eeb
Barion <ametrin.studios@gmail.com>
2025-05-22 17:58:46 +0200

    removed old test data

--- test/FileSyncTests.cs
+++
@@ -1,7 +0,0 @@
-namespace IBS.Test;
-
-public sealed class FileSyncTests
-{
-    public const string ORIGIN_PATH = "../../../Data/Origin/";
-    public const string BACKUP_PATH = "../../../Data/Backup/";
-}
