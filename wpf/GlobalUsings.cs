8e079e0f108e8674729a4a53be24f50ba2eeb042
Barion <barion+git@ik.me>
2026-09-12 22:48:24 +0200

    git settings remove wpf project

--- wpf/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-﻿global using Ametrin.Optional;
-global using Ametrin.Guards;
-global using Ametrin.Utils;
-global using IBS.Core;
-global using IBS.Core.Serialization;
-global using System;
-global using System.Collections.Generic;
-global using System.Collections.Frozen;
